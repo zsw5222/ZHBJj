218041dd707780ddf3df0a434805c8c6fb2d2f04
wjl <421007329@qq.com>
2017-07-31 21:22:10 +0800

    ssss

--- app/src/main/java/com/example/chmanw/zhbj/Splash.java
+++ app/src/main/java/com/example/chmanw/zhbj/Splash.java
@@ -1,13 +1,15 @@
 package com.example.chmanw.zhbj;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+public class Splash extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
     }
 }
---
+++ app/src/main/java/com/example/chmanw/zhbj/utils/PrefrenceTool.java
@@ -0,0 +1,12 @@
+package com.example.chmanw.zhbj.utils;
+
+import android.app.Application;
+
+/**
+ * Created by CHManW on 17/7/31.
+ */
+
+public class PrefrenceTool {
+
+
+}
